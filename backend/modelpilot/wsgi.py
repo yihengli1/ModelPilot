4c835f8fbebfd8b65c55f1901168af6dda1ccda7
Yiheng Li <105264876+YiggidyYi@users.noreply.github.com>
2025-12-13 13:19:29 -0800

    Added vercel build for production

--- backend/modelpilot/wsgi.py
+++ backend/modelpilot/wsgi.py
@@ -7,3 +7,6 @@ from django.core.wsgi import get_wsgi_application
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "modelpilot.settings")
 
 application = get_wsgi_application()
+
+# for vercel
+app = application

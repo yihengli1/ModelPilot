7efd643ab2aff93ad21919c02010a7d97f9d62c2
Yiheng Li <105264876+YiggidyYi@users.noreply.github.com>
2025-12-13 14:53:13 -0800

    Created database for AWS RDS + Toggling for prod/testing DB

--- backend/modelpilot/settings.py
+++ backend/modelpilot/settings.py
@@ -101,12 +101,30 @@ TEMPLATES = [
 WSGI_APPLICATION = "modelpilot.wsgi.application"
 ASGI_APPLICATION = "modelpilot.asgi.application"
 
-DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+if DEBUG:
+    DATABASES = {
+
+        "default": {
+            "ENGINE": "django.db.backends.sqlite3",
+            "NAME": BASE_DIR / "db.sqlite3",
+        }
     }
-}
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.mysql',
+            'NAME': os.getenv('DB_NAME'),
+            'USER': os.getenv('DB_USER'),
+            'PASSWORD': os.getenv('DB_PASSWORD'),
+            'HOST': os.getenv('DB_HOST'),
+            'PORT': os.getenv('DB_PORT', '3306'),
+            'OPTIONS': {
+                'init_command': "SET sql_mode='STRICT_TRANS_TABLES'",
+                'charset': 'utf8mb4',
+            },
+        }
+    }
+
 
 AUTH_PASSWORD_VALIDATORS = [
     {
